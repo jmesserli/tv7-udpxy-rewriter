69605064ee4afbe0bd804323b3a0e787bc02c830
Joel Messerli <hi.github@peg.nu>
2018-06-16 09:46:44 +0200

    Fix encoding problems and set sensible filename

--- src/main/kotlin/nu/peg/tv7/udpxyrewriter/config/AppConfig.kt
+++ src/main/kotlin/nu/peg/tv7/udpxyrewriter/config/AppConfig.kt
@@ -1,11 +1,22 @@
 package nu.peg.tv7.udpxyrewriter.config
 
+import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.converter.StringHttpMessageConverter
 import org.springframework.web.client.RestTemplate
+import java.nio.charset.Charset
 
 @Configuration
 class AppConfig {
     @Bean
-    fun restTemplate() = RestTemplate()
+    fun restTemplate(): RestTemplate {
+        val converter = StringHttpMessageConverter(Charset.forName("UTF-8"))
+
+        return RestTemplateBuilder()
+                .additionalMessageConverters(converter)
+                .setConnectTimeout(5000)
+                .setReadTimeout(5000)
+                .build()
+    }
 }
--- src/main/kotlin/nu/peg/tv7/udpxyrewriter/controller/PlaylistRewriteController.kt
+++ src/main/kotlin/nu/peg/tv7/udpxyrewriter/controller/PlaylistRewriteController.kt
@@ -2,6 +2,8 @@ package nu.peg.tv7.udpxyrewriter.controller
 
 import nu.peg.tv7.udpxyrewriter.service.PlaylistRewriteService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -17,7 +19,12 @@ constructor(
             @RequestParam("udpxyHost", required = true) udpxyHost: String,
             @RequestParam("udpxyPort", required = false, defaultValue = "4022") udpxyPort: Int,
             @RequestParam("https", required = false, defaultValue = "false") https: Boolean
-    ): String {
-        return playlistRewriteService.rewritePlaylist(https, udpxyHost, udpxyPort)
+    ): ResponseEntity<String> {
+        val playlist = playlistRewriteService.rewritePlaylist(https, udpxyHost, udpxyPort)
+
+        return ResponseEntity.ok()
+                .header("Content-Disposition", "attachment; filename=TV7.Udpxy.m3u")
+                .contentType(MediaType.parseMediaType("audio/mpegurl"))
+                .body(playlist)
     }
 }
